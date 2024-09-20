631c5c606f19f2a44e824d6a7eee0e135b980066
ZeeshanML <z33shanlatif007@gmail.com>
2024-09-20 11:20:53 +0500

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-ML Flow
+## ML Flow
+
+
+import dagshub
+dagshub.init(repo_owner='ZeeshanML', repo_name='ML-Flow', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import mlflow
 from mlflow.models import infer_signature
 import mlflow.sklearn
 
+import dagshub
 import logging
 
 logging.basicConfig(level=logging.WARN)
@@ -80,8 +81,8 @@ if __name__ == "__main__":
 
         ## For Remote server only(DAGShub)
 
-        # remote_server_uri="https://dagshub.com/krishnaik06/mlflowexperiments.mlflow"
-        # mlflow.set_tracking_uri(remote_server_uri)
+        remote_server_uri="https://dagshub.com/ZeeshanML/ML-Flow.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
